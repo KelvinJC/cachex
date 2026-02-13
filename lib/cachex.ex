cd0d5ed7b82251bd08e84bcb7bcc768d3c9ad362
kaizen <ojkelvinchu@gmail.com>
2026-02-13 17:36:19 +0100

    Remove unwanted code

--- lib/cachex.ex
+++ lib/cachex.ex
@@ -2,17 +2,4 @@ defmodule Cachex do
   @moduledoc """
   Documentation for `Cachex`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cachex.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
